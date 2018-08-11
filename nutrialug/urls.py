ff30a4ffb6dd61aedfa4517b73243eb93f67a3f2
Gabriele Labita <gabriele.labita@linux.it>
2018-08-11 18:04:04 +0200

    Added rest call to get last surveys.

--- nutrialug/urls.py
+++ nutrialug/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .import views
 
 urlpatterns = [
+    path('get_last', views.get_last),
     path('admin/', admin.site.urls),
     path('', include('openair.urls')),
 ]
